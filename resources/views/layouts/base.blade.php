8aae160811eeee334c171d3fd494f9cfff5db9f3
HasanHozgur <71926583+HasanHozgur@users.noreply.github.com>
2022-06-13 11:35:06 +0300

    first commit

--- resources/views/layouts/base.blade.php
+++ resources/views/layouts/base.blade.php
@@ -37,7 +37,7 @@
 						<div class="topbar-menu left-menu">
 							<ul>
 								<li class="menu-item" >
-									<a title="Hotline: (+123) 456 789" href="#" ><span class="icon label-before fa fa-mobile"></span>Hotline: (+123) 456 789</a>
+									<a title="Hotline: (+123) 456 789" href="#" ><span class="icon label-before fa fa-mobile"></span>Hotline: (+852) 456 789</a>
 								</li>
 							</ul>
 						</div>
